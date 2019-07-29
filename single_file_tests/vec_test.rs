0e09d52d7c79bb0cc2c3912b824ec974a17dd717
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-07-29 08:45:33 +0800

    update vec_test.rs

--- single_file_tests/vec_test.rs
+++ single_file_tests/vec_test.rs
@@ -1,12 +1,20 @@
 
 fn main() {
-    let v = vec![1,3,5];
+    let v = vec![1, 3, 5];
+    println!("For each print:");
     for x in &v {
         println!("{}", x);
     }
 
+    println!("Enumerate for each print:");
     for (i, x) in v.iter().enumerate() {
         println!("{} -> {}", i, x);
+    }
+
+    println!("Pop print:");
+    let mut v2 = vec![2, 4, 6];
+    while let Some(x) = v2.pop() {
+        println!("{}", x);
     }
 }
 
